1bbedbb6009e88e34daf53bba526c84807778c14
raphalcao <raphalcao@gmail.com>
2020-11-17 16:01:53 -0300

    including view

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+    
+    <div class="kt-footer__copyright">
+        2020&nbsp;&copy;&nbsp;<a href="#" target="_blank" class="kt-link">Sat Company</a>
+    </div>
+
+    <div class="kt-footer__menu">
+        <a href="https://www.satcompany.com.br/" target="_blank" class="kt-footer__menu-link kt-link">About</a>
+        <a href="https://www.satcompany.com.br/" target="_blank" class="kt-footer__menu-link kt-link">Team</a>
+        <a href="https://www.satcompany.com.br/" target="_blank" class="kt-footer__menu-link kt-link">Contact</a>
+    </div>
