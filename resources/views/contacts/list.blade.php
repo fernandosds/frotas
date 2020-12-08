45547f60cb11727c3bbfd276eb3fa6fc400ba2cf
raphalcao <raphalcao@gmail.com>
2020-12-08 09:56:27 -0300

    update list contacts

--- resources/views/contacts/list.blade.php
+++ resources/views/contacts/list.blade.php
@@ -1,35 +1,40 @@
 <table class="table table-hover">
     <thead>
         <tr>
+            <th scope="col">ID</th>
             <th scope="col">Telefone</th>
             <th scope="col">Email</th>
             <th scope="col"></th>
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <th><input type="text" id="phone" name="phone" /></th>
-            <th><input type="text" id="email" name="email" /></th>
-            <th>
-            <button type="button" class="btn btn-sm btn-outline-info"> <span class="fa fa-fw fa-edit"></span> Cadastrar</button>
-                
-                <button type="button" class="btn btn-sm  btn-outline-danger btn-delete-contact">
-                    <span class="fa fa-fw fa-trash"></span> Deletar
-                </button>
-            </th>
+        @foreach ($contacts as $contact)
+        <tr id="_tr_user_{{$contact->id}}">
+            <th scope="row">{{$contact->id}}</th>
+            <td>{{$contact->phone}}</td>
+            <td>{{$contact->email}}</td>
+            <td></td>
+            <td>
+                <div class="pull-right">
+                    <button type="button" class="btn btn-sm  btn-outline-danger btn-delete-contact" data-id="{{$contact->id}}">
+                        <span class="fa fa-fw fa-trash"></span> Deletar
+                    </button>
+                </div>
+            </td>
         </tr>
-
+        @endforeach
     </tbody>
 </table>
 
-
 @section('scripts')
-<script>
-    /* Deletar */
-    $('.btn-delete-contact').click(function() {
-        var id = $(this).data('id');
-        var url = "{{url('customers/contacts/delete')}}/" + id;
-        ajax_delete(id, url)
-    })
-</script>
+    <script>
+
+        /* Deletar */
+        $('.btn-delete-contact').click(function(){
+            var id = $(this).data('id');
+            var url = "{{url('customers/contacts/delete')}}/"+id;
+            ajax_delete(id, url)
+        })
+
+    </script>
 @endsection
