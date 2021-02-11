3886d4b753d337f9c9039b0d09c2398c73419af7
Paulo <paulosergiophp@gmail.com>
2021-02-11 08:19:51 -0300

    fix

--- app/Services/ApiDeviceService.php
+++ app/Services/ApiDeviceService.php
@@ -27,7 +27,7 @@ class ApiDeviceService
     public function __construct()
     {
 
-        if( false ){//env('APP_ENV') == "local" ){
+        if( env('APP_ENV') == "local" ){
             $this->host_siscon = "http://10.20.3.84:83/siscon/new-siscon/public/";
             $this->host = "http://10.20.3.36:6524";
         }else{
