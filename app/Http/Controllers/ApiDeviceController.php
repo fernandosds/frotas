99330819adcbe345d4666ff2e5dfe0a7fa8b2e00
Paulo <paulosergiophp@gmail.com>
2021-02-23 14:09:09 -0300

    fix

--- app/Http/Controllers/ApiDeviceController.php
+++ app/Http/Controllers/ApiDeviceController.php
@@ -49,27 +49,33 @@ class ApiDeviceController
         if( $last_position['status'] == "sucesso" ){
 
             $return["status"] = "sucesso";
-            $return["Atualizado"] =      $last_position['body'][0]["Atualizado"];
-            $return["Bateria_Violada"] = $last_position['body'][0]["Bateria_Violada"];
-            $return["CODIGO"] =          $last_position['body'][0]["CODIGO"];
-            $return["Chave"] =           $last_position['body'][0]["Chave"];
-            $return["Color"] =           $last_position['body'][0]["Color"];
-            $return["Data_GPS"] =        $last_position['body'][0]["Data_GPS"];
-            $return["Data_Rec"] =        $last_position['body'][0]["Data_Rec"];
-            $return["ENDERECO"] =        $last_position['body'][0]["ENDERECO"];
-            $return["Evt"] =             $last_position['body'][0]["Evt"];
-            $return["ID"] =              $last_position['body'][0]["ID"];
-            $return["Latitude"] =        $last_position['body'][0]["Latitude"];
-            $return["Longitude"] =       $last_position['body'][0]["Longitude"];
-            $return["Modo"] =            $last_position['body'][0]["Modo"];
-            $return["PT_ID"] =           $last_position['body'][0]["PT_ID"];
-            $return["Satelites"] =       $last_position['body'][0]["Satelites"];
-            $return["Sinal"] =           $last_position['body'][0]["Sinal"];
-            $return["Temp"] =            $last_position['body'][0]["Temp"];
-            $return["Tensão"] =          $last_position['body'][0]["Tensão"];
-            $return["Velocidade"] =      $last_position['body'][0]["Velocidade"];
-            $return["jammer"] =          $last_position['body'][0]["jammer"];
-            $return["last_address"] =    $this->apiDeviceService->getAddress($last_position['body'][0]["Latitude"], $last_position['body'][0]["Longitude"]);
+            $return["Atualizado"] =      isset( $last_position['body'][0]["Atualizado"] ) ? : '';
+            $return["Bateria_Violada"] = isset( $last_position['body'][0]["Bateria_Violada"] ) ? : '';
+            $return["CODIGO"] =          isset( $last_position['body'][0]["CODIGO"] ) ? : '';
+            $return["Chave"] =           isset( $last_position['body'][0]["Chave"] ) ? : '';
+            $return["Color"] =           isset( $last_position['body'][0]["Color"] ) ? : '';
+            $return["Data_GPS"] =        isset( $last_position['body'][0]["Data_GPS"] ) ? : '';
+            $return["Data_Rec"] =        isset( $last_position['body'][0]["Data_Rec"] ) ? : '';
+            $return["ENDERECO"] =        isset( $last_position['body'][0]["ENDERECO"] ) ? : '';
+            $return["Evt"] =             isset( $last_position['body'][0]["Evt"] ) ? : '';
+            $return["ID"] =              isset( $last_position['body'][0]["ID"] ) ? : '';
+            $return["Latitude"] =        isset( $last_position['body'][0]["Latitude"] ) ? : '';
+            $return["Longitude"] =       isset( $last_position['body'][0]["Longitude"] ) ? : '';
+            $return["Modo"] =            isset( $last_position['body'][0]["Modo"] ) ? : '';
+            $return["PT_ID"] =           isset( $last_position['body'][0]["PT_ID"] ) ? : '';
+            $return["Satelites"] =       isset( $last_position['body'][0]["Satelites"] ) ? : '';
+            $return["Sinal"] =           isset( $last_position['body'][0]["Sinal"] ) ? : '';
+            $return["Temp"] =            isset( $last_position['body'][0]["Temp"] ) ? : '';
+            $return["Tensão"] =          isset( $last_position['body'][0]["Tensão"] ) ? : '';
+            $return["Velocidade"] =      isset( $last_position['body'][0]["Velocidade"] ) ? : '';
+            $return["jammer"] =          isset( $last_position['body'][0]["jammer"] ) ? : '';
+
+            if(isset( $last_position['body'][0]["Latitude"] ) && isset( $last_position['body'][0]["Longitude"] ) ){
+                $return["last_address"] = $this->apiDeviceService->getAddress($last_position['body'][0]["Latitude"], $last_position['body'][0]["Longitude"]);
+            }else{
+                $return["last_address"] = '';
+            }
+
 
             return $return;
         }else{
