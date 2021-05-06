25cb89bec0906d87ee6d9497261931096ce4682a
Paulo <paulosergiophp@gmail.com>
2021-05-06 14:10:23 -0300

    fix

--- app/Services/ApiDeviceService.php
+++ app/Services/ApiDeviceService.php
@@ -136,6 +136,7 @@ class ApiDeviceService
     public function printGrid(String $device, String $from, String $to)
     {
 
+        //return '';
         $url = $this->host . "/hospedeiros&{$device}[type=ADDR,PER='{$from}/{$to}']";
         return ClientHttp($url);
     }
