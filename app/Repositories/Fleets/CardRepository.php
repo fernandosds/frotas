a87429799a65738bb52adc3595ba481e74c4cb2f
Paulo <paulosergiophp@gmail.com>
2021-06-02 16:22:23 -0300

    fix

--- app/Repositories/Fleets/CardRepository.php
+++ app/Repositories/Fleets/CardRepository.php
@@ -15,6 +15,7 @@ class CardRepository extends AbstractRepository
      * UserRepository constructor.
      * @param Card $model
      *
+     *
      */
     public function __construct(Card $model)
     {
--- app/Services/CommandService.php
+++ app/Services/CommandService.php
@@ -17,6 +17,7 @@ class CommandService
 
     /**
      * @var
+     *
      */
     protected $host;
 
