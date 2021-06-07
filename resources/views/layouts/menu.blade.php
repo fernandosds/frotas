1c569fc83a0a7afa4af09223d48e4fd4419c94b0
raphalcao <raphalcao@gmail.com>
2021-06-07 14:43:45 -0300

    correcting menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -33,7 +33,7 @@
     <div id="kt_aside_menu" class="kt-aside-menu " data-ktmenu-vertical="1" data-ktmenu-scroll="1" data-ktmenu-dropdown-timeout="500">
         <ul class="kt-menu__nav ">
 
-        @if( Auth::user()->type == 'ext' )
+        @if( Auth::user()->type == 'sat' )
             <!-- CLIENTE EXTERNO ______________________________________________________________________________________________________________________ -->
                 <li class="kt-menu__section ">
                     <h4 class="kt-menu__section-text">MENU</h4>
