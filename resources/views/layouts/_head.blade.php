16efc958b4b6031fe16ce34d1f4c5c91966f3b99
raphalcao <raphalcao@gmail.com>
2021-08-03 16:25:10 -0300

    fix

--- resources/views/layouts/_head.blade.php
+++ resources/views/layouts/_head.blade.php
@@ -10,10 +10,6 @@
     <meta name="description" content="Login page example">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<<<<<<< HEAD
-
-=======
->>>>>>> main
     <!--begin::Fonts -->
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
     <script>
