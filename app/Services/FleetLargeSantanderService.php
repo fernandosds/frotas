613dc35f1eb092e6b3728b112d1782aebbd5aacc
raphalcao <raphalcao@gmail.com>
2021-09-27 17:20:29 -0300

    update

--- app/Services/FleetLargeSantanderService.php
+++ app/Services/FleetLargeSantanderService.php
@@ -10,31 +10,6 @@ use stdClass;
 class FleetLargeSantanderService extends ApiFleetLargeService
 {
 
-
-    public function allCars($hash)
-    {
-        $data = $this->carsDetails($hash);
-        return $data;
-    }
-
-    private function carsDetails($hash)
-    {
-        $cars = $this->getByHash($hash);
-        $events = $this->getByHash('c58de3ae-f519-4ec6-bd87-4e011c1cb2ea');
-        return $this->relationshipEventWithCar(collect($events), collect($cars));
-    }
-
-    private function relationshipEventWithCar(Collection $events,  $cars)
-    {
-        $newCars = [];
-        foreach ($cars as $car) {
-            $car['event'] = [];
-            if ($events->firstWhere('placa_veiculo', $car['placa'])) {
-                $car['event'] = $events->firstWhere('placa_veiculo', $car['placa']);
-            }
-            $newCars[] = $car;
-        }
-        return $newCars;
-    }
+    //
 
 }
