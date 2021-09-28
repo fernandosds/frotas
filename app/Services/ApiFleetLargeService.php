90dc99a33035cf9f3650c55d2c3cce65559e2960
Garbim <garbim7@gmail.com>
2021-09-28 01:54:40 -0300

    teste

--- app/Services/ApiFleetLargeService.php
+++ app/Services/ApiFleetLargeService.php
@@ -119,6 +119,9 @@ class ApiFleetLargeService
         foreach ($filters as $filter => $value) {
             if ($filter === 'ignicao' && $data->properties->ignicao !== $value) {
                 $filtered = false;
+                if ($value == '0') {
+                    $filtered = true;
+                }
             } elseif (
                 $filter == 'cliente_datadev' &&
                 (!empty($data->properties->{$filter}) && Carbon::createFromFormat('d/m/Y H:i:s', $data->properties->{$filter})->isSameDay() != $value) ||
