40417c7515d10598b51f7e14838679e0863c84e5
Garbim <garbim7@gmail.com>
2021-09-28 15:30:56 -0300

    gradient

--- resources/views/fleetslarge/monitoring/allcars.blade.php
+++ resources/views/fleetslarge/monitoring/allcars.blade.php
@@ -188,7 +188,7 @@
                 for (var i = 0; i < planes.length; i++) {
                     heatMarkers.push([planes[i].geometry.coordinates[1], planes[i].geometry.coordinates[0], 1]);// lat, lng, intensity
                 }
-                heat = L.heatLayer(heatMarkers, { radius: 50 , blur: 25,gradient:{1:'red'}}).addTo(map);
+                heat = L.heatLayer(heatMarkers, { radius: 50 , blur: 25,gradient:{1:'red', 0: 'blue'}}).addTo(map);
             }
         });
 
