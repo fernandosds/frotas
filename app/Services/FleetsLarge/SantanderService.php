5bd82a6459978fb45bdf61a2d5957519428b5495
raphalcao <raphalcao@gmail.com>
2022-08-19 14:37:00 -0300

    fix santander

--- app/Services/FleetsLarge/SantanderService.php
+++ app/Services/FleetsLarge/SantanderService.php
@@ -15,7 +15,7 @@ class SantanderService
     /**
      * @return mixed
      */
-    public function all(Int $limit = 10000)
+    public function all($limit = 10000)
     {
         $cars = $this->santander->table($limit);
 
