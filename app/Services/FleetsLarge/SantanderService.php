b1a3b162f5ad58b999641c7ba38e2eb4588d87ff
raphalcao <raphalcao@gmail.com>
2022-08-31 16:43:17 -0300

    Ajuste filtro santander

--- app/Services/FleetsLarge/SantanderService.php
+++ app/Services/FleetsLarge/SantanderService.php
@@ -16,7 +16,7 @@ class SantanderService
      * @return mixed
      */
     // public function all($limit = 25000)
-    public function all($limit = 100)
+    public function all($limit = 25000)
     {
         $cars = $this->santander->table($limit);
 
