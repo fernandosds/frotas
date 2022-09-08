23a32e6738ce2b752222f092926a5f0204f8675d
raphalcao <raphalcao@gmail.com>
2022-09-08 10:20:02 -0300

    Adicionando analise dados santander

--- resources/views/fleetslarge/iframe/dashboardSantander.blade.php
+++ resources/views/fleetslarge/iframe/dashboardSantander.blade.php
@@ -7,13 +7,13 @@
 
 @section('content')
 
-
+<br>
     <iframe
         onload='resizeIframe(this)'
         src="https://bi.satcompany.com.br/public/dashboard/{{$hash}}"
         frameborder="0"
         width="100%"
-        height="2500px"
+        height="1500px"
         allowtransparency
     ></iframe>
 
